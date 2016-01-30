ce47f0c6ebcfe92b5428ceaa125a478322c0bb4c
Mr. Hangover <ma.nygaard@webspeed.dk>
2016-01-30 16:59:05 +0100

    Started working on KILLING stuff

---
+++ Assets/Scripts/KillTrigger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+
+        }
+    }
+}
