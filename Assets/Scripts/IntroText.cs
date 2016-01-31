6ea54a494dad1ad7f02d3a4739c8d6395896a6cb
Mr. Hangover <ma.nygaard@webspeed.dk>
2016-01-31 14:23:53 +0100

    Intro and outro done!

---
+++ Assets/Scripts/IntroText.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class IntroText : MonoBehaviour {
+
+    Text canvasText;
+    public string[] displayText;
+    public string sceneToLoad;
+    public bool isLastScene = false;
+    int displayingText;
+
+    // Use this for initialization
+    void Start () {
+        canvasText = GetComponent<Text>();
+        canvasText.text = displayText[0];
+        displayingText = 0;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (displayingText < displayText.Length - 1)
+            {
+                displayingText++;
+                canvasText.text = displayText[displayingText];
+            }
+            else if(isLastScene)
+            {
+                Application.Quit();
+            }
+            else
+            {
+                UnityEngine.SceneManagement.SceneManager.LoadScene(sceneToLoad);
+            }
+        }
+	}
+}
